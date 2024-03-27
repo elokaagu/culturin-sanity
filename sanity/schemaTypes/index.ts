1d519baa57ff0f1b403c007b54c7bfc44b33a16a
Eloka Agu <eloka@satellitelabs.xyz>
2024-03-27 12:22:22 +0000

    providers in sanity

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,4 +1,5 @@
 import blog from './blog'
 import video from './video'
+import providers from './providers'
 
-export const schemaTypes = [blog, video]
+export const schemaTypes = [blog, video, providers]
