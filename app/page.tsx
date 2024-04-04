42d80e5f2a6f997fd0ebf16a03e3e7767f30cc38
Eloka Agu <eloka@satellitelabs.xyz>
2024-04-04 19:13:52 +0100

    culturin sanity

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
-import styles from "./page.module.css";
 import styled from "styled-components";
 
 export default function Home() {
